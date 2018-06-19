2e8fe936e3cda8825877c482712fdf7a4ed73231
Ahmed Samy <ahmed1abdalgaleel@gmail.com>
2018-06-19 16:40:56 +0200

    dataset

---
+++ dataset_generator.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+import cv2
+import os
+
+import face_rec_functions
+
+
+
+datasetPath='D:\Users\M_K\Documents\GitHub\Facial-recognition\dataset'
+if not os.path.exists(datasetPath):
+	os.makedirs(datasetPath)
+
+face_rec_functions.createFile()
+name=raw_input('Enter your name:')
+
+Id=face_rec_functions.dumpData(name)
+print(Id)
+
+count=0
+
+
+detector= cv2.CascadeClassifier('C:\Users\M_K\Desktop/face rec - Copy\Haarcascades/haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+while(True):
+	ret, img = cap.read()
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = detector.detectMultiScale(gray, 1.3, 5)
+
+	for (x,y,w,h) in faces:
+		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+		count+=1
+
+		cv2.imwrite(datasetPath+'/user.'+str(Id)+'.'+str(count)+".jpg",gray[y:y+h,x:x+w])
+
+
+
+
+	print(count)
+	cv2.imshow('frame',img)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+	elif count>50:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
